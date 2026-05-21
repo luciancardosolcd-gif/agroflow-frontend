46b84a1cb662a0c169f991f81ca6bad02e8acdf5
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-21 20:43:15 -0300

    Update editar[id]page.tsx

--- src/app/financeiro/editar[id]page.tsx
+++ src/app/financeiro/editar[id]page.tsx
@@ -1,1 +1,9 @@
+'use client'
+import { use } from 'react'
+import LancamentoPage from '@/components/LancamentoPage'
+
+export default function EditarLancamentoPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params)
+  return <LancamentoPage id={id} />
+}
 
