2aaed884645c23b85f25f859781a2e889d59ebab
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-21 21:26:19 -0300

    Delete src/app/financeiro/editar[id]page.tsx

--- src/app/financeiro/editar[id]page.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import LancamentoPage from '@/components/LancamentoPage'
-
-export default function EditarLancamentoPage({ params }: { params: { id: string } }) {
-  return <LancamentoPage id={params.id} />
-}
